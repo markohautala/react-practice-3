5f19b203a2abe22f2dfd2ec279e2fd685e385a98
markohautala <markohautala@live.se>
2024-06-22 15:07:40 +0200

    Update dependencies and import statements

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
-import css from './App.module.css';
-import Sidebar from "./components/Sidebar";
+import css from './App.module.css';  // Import the CSS file
+import Sidebar from "./components/Sidebar"; // Import the Sidebar component
 
 function App() {
   return (
