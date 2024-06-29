51e3d6eda5b6552f115e50b545003a57390764ac
markohautala <markohautala@live.se>
2024-06-29 18:10:52 +0300

    Refactor ES7Snippets component

--- my-app/src/components/ES7Snippets.js
+++ my-app/src/components/ES7Snippets.js
@@ -1,19 +1,42 @@
-// import React from 'react'
+// // import React from 'react'
 
-// function ES7Snippets() {
-//   return (
-//     <div>ES7Snippets</div>
-//   )
+// // function ES7Snippets() {
+// //   return (
+// //     <div>ES7Snippets</div>
+// //   )
+// // }
+
+// // export default ES7Snippets
+
+// // import React from 'react'
+
+// // const ES7Snippets = () => {
+// //   return (
+// //     <div>ES7Snippets</div>
+// //   )
+// // }
+
+// // export default ES7Snippets
+
+// import React, { Component } from 'react'
+
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
 // }
 
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// console.log(first)
+// console.log('first', first)
+
 // export default ES7Snippets
-
-import React from 'react'
-
-const ES7Snippets = () => {
-  return (
-    <div>ES7Snippets</div>
-  )
-}
-
-export default ES7Snippets
