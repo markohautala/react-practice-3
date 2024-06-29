4166921170caeb6d64e62ecc51461f8e3e6af85c
markohautala <markohautala@live.se>
2024-06-29 08:12:36 +0300

    Install ES7 och use it

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,19 @@
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+import React from 'react'
+
+const ES7Snippets = () => {
+  return (
+    <div>ES7Snippets</div>
+  )
+}
+
+export default ES7Snippets
